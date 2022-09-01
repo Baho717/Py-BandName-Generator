0df36d9ad61e94d8465ae17334ea4e8ef0418f6a
Benjamin Aho <Benjamin.aho27@gmail.com>
2022-08-31 22:38:10 -0400

    files completed, in the future, add a loop

---
+++ Py-BandName-Generator.py
@@ -0,0 +1,5 @@
+print("Welcome to Py Band Name Generator!")
+city = input("Please Write the name of your Home Town.")
+pet = input("What's the name of a pet?")
+final = city + " " + pet
+print("Your band name should be " + final)
